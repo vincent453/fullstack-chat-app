05b8329fd297c86aed161d4012ac6a6a79766860
lawrence vincent <93725993+vincent453@users.noreply.github.com>
2025-05-28 19:36:34 +0100

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -13,20 +13,65 @@ const io = new Server(server, {
 
 export function getReceiverSocketId(userId) {
     return userSocketMap[userId];
-  }
-  
+}
 
 // used to store online users
 const userSocketMap = {}; // {userId: socketId}
+const userNotificationPreferences = {}; // Store user notification preferences
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+    
+    // Set default notification preferences if not exists
+    if (!userNotificationPreferences[userId]) {
+      userNotificationPreferences[userId] = {
+        sound: true,
+        desktop: true,
+        inApp: true
+      };
+    }
+  }
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Handle notification preferences update
+  socket.on("updateNotificationPreferences", (preferences) => {
+    if (userId) {
+      userNotificationPreferences[userId] = { ...userNotificationPreferences[userId], ...preferences };
+      socket.emit("notificationPreferencesUpdated", userNotificationPreferences[userId]);
+    }
+  });
+
+  // Handle notification read status
+  socket.on("markNotificationAsRead", (notificationId) => {
+    // You could store this in database for persistence
+    socket.emit("notificationMarkedAsRead", notificationId);
+  });
+
+  // Handle typing indicators
+  socket.on("typing", (data) => {
+    const receiverSocketId = getReceiverSocketId(data.receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("userTyping", {
+        senderId: userId,
+        senderName: data.senderName
+      });
+    }
+  });
+
+  socket.on("stopTyping", (data) => {
+    const receiverSocketId = getReceiverSocketId(data.receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("userStoppedTyping", {
+        senderId: userId
+      });
+    }
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
@@ -35,4 +80,4 @@ io.on("connection", (socket) => {
   });
 });
 
-export { io, app, server };
+export { io, app, server, userNotificationPreferences };
