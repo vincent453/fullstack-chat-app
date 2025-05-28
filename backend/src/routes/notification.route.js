96b046b42da15177e6607ec73011e1f2649a6449
lawrence vincent <93725993+vincent453@users.noreply.github.com>
2025-05-28 19:43:00 +0100

    Update notification.route.js

--- backend/src/routes/notification.route.js
+++ backend/src/routes/notification.route.js
@@ -1,1 +1,18 @@
 
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { 
+  getNotifications, 
+  markAsRead, 
+  markAllAsRead, 
+  deleteNotification 
+} from "../controllers/notification.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.put("/read/:notificationId", protectRoute, markAsRead);
+router.put("/read-all", protectRoute, markAllAsRead);
+router.delete("/:notificationId", protectRoute, deleteNotification);
+
+export default router;
